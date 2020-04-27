6190806741925dca28c63942c3b354fa12e8e359
Gsumar271 <statsrats@yahoo.com>
2020-04-27 13:30:14 -0400

    New data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,12 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=224b2714834de2eabff25c3e5c627dde&query='+ latitude +','+ longitude + '&units=f'
 
     request({url, json: true}, (error, {body}) => {
-
         if (error) {
             callback("Unable to connect to location services", undefined) 
         } else if(body.error) {
             callback('Unable to find location, Try another search', undefined)
         } else {
-            const data = body.current.weather_descriptions[0] + '. It is curently ' + body.current.temperature + ' degrees. It feels like '+ body.current.feelslike + ' degrees.' 
+            const data = body.current.weather_descriptions[0] + '. It is curently ' + body.current.temperature + ' degrees. It feels like '+ body.current.feelslike + ' degrees. \n' + 'Humidity: ' + body.current.humidity
             callback (undefined, data)
        }  
     })
